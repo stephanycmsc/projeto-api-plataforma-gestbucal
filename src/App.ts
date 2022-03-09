0747417a3c0abf669164b4baf45de58d6cbc532b
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-03-08 21:25:53 -0300

    add(Server): Adiciona arquivos base do server.

---
+++ src/App.ts
@@ -0,0 +1,17 @@
+require('dotenv/config')
+import * as express from 'express'
+import { createConnection } from 'typeorm'
+
+export default class App {
+    private port: string = process.env.PORT
+    public app: express.Application
+
+    public constructor () {
+        this.app = express()
+    }
+
+    public startApp () {
+        createConnection().then(async connection => {})
+        this.app.listen(this.port, () => { console.log(`Server running in port: ${this.port}`) })
+    }
+}
