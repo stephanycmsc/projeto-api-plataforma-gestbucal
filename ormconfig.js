77a63f0e609da32f07dc48bf519ef48056ccd576
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-03-18 20:52:36 -0300

    fix(database-connection): Corrige a conexão com o banco dev da nuvem.

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     type: "postgres",
-    host: process.env.RDS_WAY_SERVER,
-    port: 5432,
-    username: "rds-server-dev",
-    password: process.env.RDS_WAY_SERVER_PWD,
-    database: process.env.RDS_WAY_SERVER_DB
+    host: process.env.RDS_SBUFPE_SERVER_HOST,
+    port: process.env.RDS_SBUFPE_SERVER_DB_PORT,
+    username: process.env.RDS_SBUFPE_SERVER_USER,
+    password: process.env.RDS_SBUFPE_SERVER_PWD,
+    database: process.env.RDS_SBUFPE_SERVER_DB
  }
