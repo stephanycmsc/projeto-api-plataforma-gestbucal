c33123a0f87e969687ca49c09b4351ebcfd398e1
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-04-22 21:32:37 -0300

    fix(user-controller): Contorna os problemas das novas entidades.

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,67 +1,65 @@
 import * as express from "express"
-import {getRepository} from "typeorm";
-import {Request, Response, Router} from "express";
-import {Users} from "../entity/Users";
-import moment from "moment";
+import { getRepository } from "typeorm";
+import { Request, Response, Router } from "express";
+import { UsersTypes } from "../entity/UsersTypes";
 
 export default class UserController {
-    getUsers = async (req: Request, res: Response): Promise<Response> => {
-        try {
-            const userRepo = getRepository(Users)
+  getUsers = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const userRepo = getRepository(UsersTypes)
 
-            /////// throw new Error('Erro forçado')
+      /////// throw new Error('Erro forçado')
 
 
-            return res.json({
-                status: 0,
-                data: await userRepo.findAndCount(),
-                message: ''
-            })
-        } catch (error) {
-            return res.json({
-                status: 1,
-                data: null,
-                message: typeof(error) === 'string' ? error : error.message
-            })
-        }
-
+      return res.json({
+        status: 0,
+        data: await userRepo.findAndCount(),
+        message: ''
+      })
+    } catch (error) {
+      return res.json({
+        status: 1,
+        data: null,
+        message: typeof (error) === 'string' ? error : error.message
+      })
     }
 
-    addUser = async (req: Request, res: Response): Promise<Response> => {
-        try {
-            const userRepo = getRepository(Users)
-            const result = await userRepo.save({
-                age: req.body.age,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName
-            })
+  }
 
-            return res.json(result)
-        } catch (error) {
-            return res.json(typeof(error) === 'object' ? error.message : error)//NATHAN
-        }
+  addUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const userRepo = getRepository(UsersTypes)
+      // const result = await userRepo.save({
+      //   firstName: req.body.firstName,
+      //   lastName: req.body.lastName
+      // })
+
+      return res.json()
+    } catch (error) {
+      return res.json(typeof (error) === 'object' ? error.message : error)//NATHAN
     }
+  }
 
-    deleteUser = async (req: Request, res: Response): Promise<Response> => {
-        try {
-            const userRepo = getRepository(Users)
-            const result = await userRepo.delete({ id: 6 }) //params
+  deleteUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const userRepo = getRepository(UsersTypes)
+      const result = await userRepo.delete({ id: 6 }) //params
 
-            return res.json({})
-        } catch (error) {
-            return res.json(typeof(error) === 'object' ? error.message : error)//NATHAN
-        }
+      return res.json({})
+    } catch (error) {
+      return res.json(typeof (error) === 'object' ? error.message : error)//NATHAN
     }
+  }
 
-    update = async (req: Request, res: Response): Promise<Response> => {
-        const userRepo = await getRepository(Users).findOne(req.params.id)
-        getRepository(Users).merge(userRepo, req.body)
-    }
+  update = async (req: Request, res: Response) => {
+    const userRepo = await getRepository(UsersTypes).findOne(req.params.id)
+    getRepository(UsersTypes).merge(userRepo, req.body)
+  }
 
 
-    setRoutes(router: Router) {
-        router.get('/users', this.getUsers)
-        router.post('/users', this.addUser)
-        router.delete('/users', this.deleteUser)
-    }
+  setRoutes(router: Router) {
+    router.get('/users', this.getUsers)
+    router.post('/users', this.addUser)
+    router.delete('/users', this.deleteUser)
+  }
 }
