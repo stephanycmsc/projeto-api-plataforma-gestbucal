a67dedf92e47facf3d4d4740393e62e8390baca7
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-04-22 21:29:06 -0300

    add(users-address): Nullable fields.

--- src/entity/UsersAddress.ts
+++ src/entity/UsersAddress.ts
@@ -6,21 +6,21 @@ export class UsersAddress {
   @PrimaryGeneratedColumn()
   id: ID;
 
-  @Column({ name: 'cep', unique: false, length: 8 })
+  @Column({ name: 'cep', unique: false, length: 8, nullable: false })
   cep: string;
 
-  @Column({ name: 'street', unique: false, length: 32 })
+  @Column({ name: 'street', unique: false, length: 32, nullable: false })
   street: string;
 
-  @Column({ name: 'street_numberr', unique: false, length: 32 })
+  @Column({ name: 'street_number', unique: false, length: 32, nullable: false })
   street_number: string;
 
-  @Column({ name: 'city', unique: false, length: 20 })
+  @Column({ name: 'city', unique: false, length: 20, nullable: false })
   city: string;
 
-  @Column({ name: 'district', unique: false, length: 20 })
+  @Column({ name: 'district', unique: false, length: 20, nullable: false })
   district: string;
 
-  @Column({ name: 'complement', unique: false, length: 32 })
+  @Column({ name: 'complement', unique: false, length: 32, nullable: true })
   complement: string;
 }
