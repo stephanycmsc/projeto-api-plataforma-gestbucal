0ffd9cd82a4010c096a490f784c2cd610bd5b75a
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-05-25 00:54:10 -0300

    add(types): Adiciona a tipagem aos requests.

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,12 @@
+import { Query, Send } from 'express-static-serve-core'
+
 export type ID = number
+
+export interface TypedReq<T extends Query, U> extends Express.Request {
+  body: U,
+  query: T
+}
+
+export interface TypedRes<ResBody> extends Express.Response {
+  json: Send<ResBody, this>;
+}
